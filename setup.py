6120dcb94c17bfc90be0984f93430c274b81101e
Randy Zwitch <rzwitch@gmail.com>
2021-05-27 14:24:50 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-letsplot",
+    version="0.0.1",
+    author="Randy Zwitch",
+    author_email="randy@streamlit.io",
+    description="Streamlit component for Lets Plot visualization library",
+    long_description="Streamlit component for Lets Plot visualization library",
+    long_description_content_type="text/plain",
+    url="https://github.com/randyzwitch/streamlit-letsplot",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=["streamlit >= 0.63", "lets-plot"],
+)
